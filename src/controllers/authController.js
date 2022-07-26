6cfdf0402feaf71184c9542cb7d246464e1ae925
RealWorldd <jinpark99@naver.com>
2022-07-27 03:09:40 +0900

    fix: fix jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import fetch from "cross-fetch";
 export const refreshToken = async (req, res, next) => {
   const refreshToken = req.headers.refreshToken;
   console.log(refreshToken);
-  const accessToken = req.body.accessToken;
+  const accessToken = req.headers.accessToken;
   console.log(accessToken);
   if (!refreshToken) return res.status(401).json("You are not authenticated!");
   const userRefreshToken = await Token.findOne({ refreshToken });
