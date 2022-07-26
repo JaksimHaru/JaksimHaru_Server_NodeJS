a04183b01f97987cba0df75e24e452792c88ebf9
RealWorldd <jinpark99@naver.com>
2022-07-27 00:45:45 +0900

    fix: fix jwt

--- src/server.js
+++ src/server.js
@@ -14,15 +14,15 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
-app.use(function(req, res, next) {
-  res.header('Content-Type', 'application/json;charset=UTF-8')
-  res.header('Access-Control-Allow-Credentials', true)
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 
