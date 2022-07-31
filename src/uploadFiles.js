ca8ad59747ea72ae3806567a3b85a202257eaa6f
RealWorldd <jinpark99@naver.com>
2022-08-01 00:45:47 +0900

    test: test code

--- src/uploadFiles.js
+++ src/uploadFiles.js
@@ -12,7 +12,7 @@ const s3 = new aws.S3({
 const s3ImageUploader = multerS3({
   s3: s3,
   bucket: "jaksimharu",
-  acl: "public-read",
+  acl: "public-read-write",
 });
 
 export const imageUpload = multer({
