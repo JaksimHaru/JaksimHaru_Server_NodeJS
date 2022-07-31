46165edeabc14d259b06848050b7827806fed1f5
RealWorldd <jinpark99@naver.com>
2022-07-31 23:50:53 +0900

    test: test code

--- src/verifyToken.js
+++ src/verifyToken.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 import { createError } from "./error";
 
 export const verifyToken = (req, res, next) => {
-  console.log(req.headers);
   const accessToken = req.headers.accesstoken;
   if (!accessToken) {
     return next(createError(401, "로그인이 필요합니다"));
