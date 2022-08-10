7ad7eccbc46165dd269cf6f3bf5a5824055fd66e
RealWorldd <jinpark99@naver.com>
2022-08-11 03:01:07 +0900

    test: test code

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,12 +1,16 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const test = (req, res, next) => {
-  const refreshToken = jwt.sign({}, process.env.JWT, {
-    algorithm: "HS256",
-    expiresIn: "14d",
-  });
-  res.status(200).cookie("refresh_token", refreshToken, {
-    httpOnly: true,
-    secure: true,
-  });
+  try {
+    const refreshToken = jwt.sign({}, process.env.JWT, {
+      algorithm: "HS256",
+      expiresIn: "14d",
+    });
+    res.status(200).cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: true,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
