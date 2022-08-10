c1529b1660ea0accf021b2ab285c1b9b38984319
RealWorldd <jinpark99@naver.com>
2022-08-11 02:22:22 +0900

    test: test code

--- src/routes/globalRoutes.js
+++ src/routes/globalRoutes.js
@@ -3,6 +3,10 @@ import express from "express";
 const globalRoutes = express.Router();
 
 globalRoutes.get("/", (req, res) => {
+  const refreshToken = jwt.sign({}, process.env.JWT, {
+    algorithm: "HS256",
+    expiresIn: "14d",
+  });
   res.status(200).cookie("refresh_token", refreshToken, {
     httpOnly: true,
     secure: true,
