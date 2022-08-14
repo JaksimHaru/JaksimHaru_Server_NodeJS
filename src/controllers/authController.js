05aed122b5a9df96dd5d8638646e6d09f2564a11
RealWorldd <jinpark99@naver.com>
2022-08-14 23:26:31 +0900

    test: test code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -127,6 +127,7 @@ export const signin = async (req, res, next) => {
     res
       .status(200)
       .cookie("refresh_token", refreshToken, {
+        httpOnly: true,
         secure: true,
         sameSite: "None",
       })
--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(
 );
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "https://jaksimharu.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
   "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
