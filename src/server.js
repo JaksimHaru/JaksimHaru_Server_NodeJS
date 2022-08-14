68ad1af7abb34e4e5ddacf8d3e9a373423a29924
RealWorldd <jinpark99@naver.com>
2022-08-14 21:40:55 +0900

    fix: res headers

--- src/server.js
+++ src/server.js
@@ -22,16 +22,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(function (req, res, next) {
-  res.header("Content-Type", "application/json;charset=UTF-8");
-  res.header("Access-Control-Allow-Origin", "https://jaksimharu.netlify.app");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(cookieParser());
 app.use(express.json());
 
